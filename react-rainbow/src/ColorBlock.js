c7e8439170be3a68a2c5926079d72d3bc1a0498c
Faustino Ruiz <ftr1989@gmail.com>
2024-04-02 16:50:14 -0700

    4:50pm

---
+++ react-rainbow/src/ColorBlock.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+// Make sure to import the component we just built:
+import ColorBlock from './ColorBlock'
+
+function App(){
+    let colors = [
+        'violet', 'blue',
+        'lightblue', 'green',
+        'greenyellow', 'yellow',
+        'orange', 'red'
+    ]
+    
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    return (
+        <div className="App">
+            {colorMap}
+        </div>
+    )
+}
+
+
+// Do not forget to export your component once you have built it!
+export default ColorBlock
