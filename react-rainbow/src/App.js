cd4a70ea5ba138db774750df408cca242fdcb0f2
Faustino Ruiz <ftr1989@gmail.com>
2024-04-21 19:56:25 -0700

    indexcss

--- react-rainbow/src/App.js
+++ react-rainbow/src/App.js
@@ -1,15 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import React, { useState } from 'react'
+import { useState } from 'react'
+import ColorBlock from "./Components/ColorBlock";
+import ColorForm from "./Components/ColorForm";
 
-function App(){
-    let colors = ['violet', 'blue', 'lightblue', 'green', 'greenyellow', 'yellow', 'orange', 'red']
-    return (
-        <div className="App">
 
-        </div>
-    );
+function App() {
+  const [colors, setColors] = useState([]);
+
+  const addColor = (newColor) => {
+    setColors([...colors, newColor])
+  }
+
+  const colorMap = colors.map((color, i) => {
+    return <ColorBlock key={i} color={color} />;
+  });
+
+  return (
+  <div className="App centered-content">
+    <h1>add your colors!</h1>
+    {colorMap}
+  <ColorForm addColor={addColor}/>
+  </div>
+  )
 }
-
 
 export default App;
